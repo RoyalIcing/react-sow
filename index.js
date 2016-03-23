a500413837b81160fcf2ac8a692a7fcdd067472e
Patrick Smith <pgwsmith@gmail.com>
2016-03-23 15:39:03 +1100

    Allow 'classes', an array of class names or functions
    Style prop 'classes' is an array. Its items can either be class name strings, or functions that return a class name string. The function is called when rendered, allowing a stylesheet to be loaded or inserted on the fly.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function createStyler(renderStyle, children) {
 	return Object.assign(
 		function(props) {
+			// Render
+			var outputStyle = renderStyle(props || {});
+			// Make copy, with custom props merged
+			outputStyle = Object.assign({}, 
+				outputStyle,
+				!!props ? props.style : undefined
+			);
+			
+			var outputClassName = (props && props.className) || '';
+			
+			// Use classes, if present
+			if (outputStyle.classes) {
+				outputStyle.classes.forEach(function(useClass) {
+					var className;
+					if (typeof useClass === 'string') {
+						className = useClass;
+					}
+					else {
+						className = useClass();
+					}
+					
+					outputClassName += ' ' + className;
+				});
+				
+				// Remove from outputted rules
+				delete outputStyle.classes;
+			}
+			
+			// Return output props
 			return {
-				style: Object.assign({},
-					renderStyle(props || {}),
-					!!props ? props.style : undefined
-				)
+				style: outputStyle,
+				className: outputClassName
 			};
 		},
 		children
