158f5e646fb47e62437624870fcaff11fb888543
Patrick Smith <pgwsmith@gmail.com>
2016-03-23 11:58:10 +1100

    Add sow.combine() for combining stylers

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 function createStyler(renderStyle, children) {
 	return Object.assign(
 		function(props) {
-            return {
-                style: Object.assign({},
-                    renderStyle(props || {}),
-                    !!props ? props.style : undefined
-                )
-            };
-        },
+			return {
+				style: Object.assign({},
+					renderStyle(props || {}),
+					!!props ? props.style : undefined
+				)
+			};
+		},
 		children
 	);
 }
+
+function combine(stylers) {
+	return function(props) {
+		return stylers.reduce(function(combined, styler) {
+			var output = styler(props);
+			Object.assign(combined.style, output.style);
+			return combined;
+		}, { style: {} });
+	}
+}
+
+createStyler.combine = combine;
 
 module.exports = createStyler;
