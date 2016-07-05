feb7cbaf2c146a6aa5a6acf694633d08f4559d4f
Patrick Smith <pgwsmith@gmail.com>
2016-07-05 16:16:25 +1000

    Allow passing of premade sow instances

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
 var isFunction = require('lodash.isfunction');
 
 
-function createStyler(renderStyle, children) {
+function createStyler(renderStyle) {
 	if (!isFunction(renderStyle)) {
 		var style = renderStyle;
 		renderStyle = function() {
 			return style;
 		};
 	}
-	
-	return Object.assign(
-		function(props) {
-			// Render
-			var outputStyle = renderStyle(props || {});
-			// Make copy, with custom props merged
-			outputStyle = Object.assign({}, 
-				outputStyle,
-				!!props ? props.style : undefined
-			);
-			
-			var outputClassName = (props && props.className) || '';
-			
-			// Use classes, if present
-			if (outputStyle.classes) {
-				outputStyle.classes.forEach(function(useClass) {
-					var className;
-					if (typeof useClass === 'string') {
-						className = useClass;
-					}
-					else {
-						className = useClass();
-					}
-					
-					outputClassName += ' ' + className;
-				});
+
+	return function renderProps(props) {
+		// Render
+		var outputStyle = renderStyle(props || {});
+		// Make copy, with custom props merged
+		outputStyle = Object.assign({}, 
+			outputStyle,
+			!!props ? props.style : undefined
+		);
+		
+		var outputClassName = (props && props.className) || '';
+		
+		// Use classes, if present
+		if (outputStyle.classes) {
+			outputStyle.classes.forEach(function(useClass) {
+				var className;
+				if (typeof useClass === 'string') {
+					className = useClass;
+				}
+				else {
+					className = useClass();
+				}
 				
-				// Remove from outputted rules
-				delete outputStyle.classes;
-			}
+				outputClassName += ' ' + className;
+			});
 			
-			// Return output props
-			return {
-				style: outputStyle,
-				className: outputClassName
-			};
-		},
+			// Remove from outputted rules
+			delete outputStyle.classes;
+		}
+		
+		// Return output props
+		return {
+			style: outputStyle,
+			className: outputClassName
+		};
+	}
+}
+
+function sow(renderStyle, children) {
+	return Object.assign(
+		!!renderStyle.isSow ? (
+			renderStyle
+		) : (
+			createStyler(renderStyle)
+		),
+		{ isSow: true },
 		children
 	);
 }
@@ -53,7 +62,7 @@ function combine(stylers) {
 	var newStyler = function(props) {
 		// Flatten 'style' prop
 		return stylers.reduce(function(combined, styler) {
-			var output = styler(props);
+			var output = sow(styler)(props);
 			Object.assign(combined.style, output.style);
 			return combined;
 		}, { style: {} });
@@ -65,6 +74,5 @@ function combine(stylers) {
 	return newStyler;
 }
 
-createStyler.combine = combine;
-
-module.exports = createStyler;
+sow.combine = combine;
+module.exports = sow;
