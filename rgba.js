fd9f0d8d00778c2f377891b21332e3471284befd
Patrick Smith <pgwsmith@gmail.com>
2016-07-05 16:05:31 +1000

    Add ability to transform rgba components

--- rgba.js
+++ rgba.js
@@ -5,10 +5,30 @@ function RGBA(r, g, b, a) {
 	this.a = (typeof a === 'undefined' ? 1 : a);
 }
 
+RGBA.prototype.mapRGB = function(mapper) {
+	return new RGBA(
+		mapper(this.r, 'r'),
+		mapper(this.g, 'g'),
+		mapper(this.b, 'b'),
+		this.a
+	)
+}
+
+RGBA.prototype.mapAlpha = function(mapper) {
+	return new RGBA(
+		this.r,
+		this.g,
+		this.b,
+		mapper(this.a)
+	)
+}
+
+// Used by React to automatically convert to a string
 RGBA.prototype.toString = function() {
 	return 'rgba(' + [this.r, this.g, this.b, this.a].join(',') + ')';
 };
 
+// We want the underlying RGBA type to be an implementation detail
 function rgba(r, g, b, a) {
 	return new RGBA(r, g, b, a);
 }
